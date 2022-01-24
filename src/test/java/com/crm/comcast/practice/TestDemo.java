1a9dd51ac731103b0dd4b3404f6c3e93cb3c676d
Dell <Dell@DESKTOP-HN4067J>
2022-01-24 18:37:17 +0530

    1st commit

---
+++ src/test/java/com/crm/comcast/practice/TestDemo.java
@@ -0,0 +1,11 @@
+package com.crm.comcast.practice;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+@Test
+public static void demoMethod() {
+	System.out.println("User 1 commit");
+}
+
+}
