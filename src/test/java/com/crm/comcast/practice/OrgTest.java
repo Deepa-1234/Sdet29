716d966ccd6082f1fc09aefeac64fab30511dc04
Anju <Anju@DESKTOP-N0KKL74>
2022-01-26 16:11:57 +0530

    SAnthosh2: made chnages second time

--- src/test/java/com/crm/comcast/practice/OrgTest.java
+++ src/test/java/com/crm/comcast/practice/OrgTest.java
@@ -6,12 +6,13 @@ public class OrgTest {
 @Test(groups = "ST")
 public static void orgTest() {
 	System.out.println("org created");
-	System.out.println("SANTHOSH: made this chnages after forking Deepa's Master branch to learn branching");
+	System.out.println("SANTHOSH1: made this chnages after forking Deepa's Master branch to learn branching");
 }
 
 @Test(groups = "IT")
 public void orgWithIndustryTest() {
 	System.out.println("org with industry");
+	System.out.println("SANTHOSH2: made this SECOND chnages after forking Deepa's Master branch to learn branching");
 }
 
 }
